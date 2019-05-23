293fb51f18a4c934aebcdc60c669ca302f024e0c
Lucas Cruz dos Reis <lcr.ergo@gmail.com>
2019-05-23 11:09:32 -0300

    Consertando gauss_jacobi

--- gauss_jacobi.r
+++ gauss_jacobi.r
@@ -1,51 +1,53 @@
 ##Gauss-Jacobi
 
 gauss_jacobi <- function(A,B,xk,erro){
-    new_xk <- matrix(0,3,1);
+    new_xk <- matrix(0,3,1)
 
-    erro <- 0.00005;
-    n <- dim(B)[1];
-    k <- 0;
-    d <- erro + 1;
+    erro <- 0.00005
+    n <- dim(B)[1]
+    k <- 0
+    d <- erro + 1
+    iter <- 0
 
-    flag <- 1;  
+    flag <- 1  
 
     for (i in 1:n){
-	sum <- 0;
+	sum <- 0
 	for (j in 1:n){
 	    if (i != j)
-		sum <- sum + abs(A[i,j]);
+		sum <- sum + abs(A[i,j])
 	}
 	if (sum/abs(A[i,i]) >= 1)
-	    flag <- 0;
+	    flag <- 0
     }
 
 
     if (flag){
-	start_time <- Sys.time();
+	start_time <- Sys.time()
 	while(d > erro){
-	    k <- k + 1;
+	    k <- k + 1
 	    for (i in 1:n){
-		sum <- 0;
+		sum <- 0
 		for (j in 1:n){
 		    if (i != j)
-			sum <- sum + A[i,j] * xk[j];
+			sum <- sum + A[i,j] * xk[j]
+		    iter <- iter + 1
 		}
-		new_xk[i] <- (1/A[i,i]) * (B[i]-sum);
+		new_xk[i] <- (1/A[i,i]) * (B[i]-sum)
 	    }
 
-	    max1 <- max(abs(new_xk-xk));
-	    max2 <- max(abs(new_xk));
-	    d <- max1/max2;
+	    max1 <- max(abs(new_xk-xk))
+	    max2 <- max(abs(new_xk))
+	    d <- max1/max2
 
-	    xk <- new_xk;
+	    xk <- new_xk
 	}
 
 	end_time <- Sys.time()
-	timer <- end_time - start_time;
+	timer <- end_time - start_time
 
-	result <- c(new_xk,timer,iteracoes);
-	return (result);
+	result <- c(new_xk, timer, iter)
+	return (result)
     }else 
-	cat("Não existe solução por Gauss-Jacobi");
+	return (NA)
 }
