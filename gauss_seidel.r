d641879bebc93aacdee22be40f9a6053c2b964a5
Mayumi <kahjohansson@gmail.com>
2019-05-26 21:18:21 -0300

    Padronização do gauss seidel

--- gauss_seidel.r
+++ gauss_seidel.r
@@ -1,62 +1,77 @@
-##Gauss-Jacobi
+# Name: gauss-seidel.r
+# Desc.: Solves a linear system with the
+# Gauss-Seidel elmination method.
+# Group: 2
+# 
+# Imput: A matrix of the coeficients(A),
+# a column matrix(b), a column matrix(xk) and a error value.
+# Output: A column matrix with the solution value(X)
+# 
 
-gauss_seidel <- function(A,B,xk,erro){
-    new_xk <- matrix(0,3,1)
+# Ex: n <- 3
+# A = matrix(c(10,1,2,2,5,3,1,1,10), n, n);
+# b = matrix(c(7,-8,6), n, 1);
+# xk = matrix(c(0.7,1.4,-1.1), n, 1);
+# error = 0.00005;
+#
+# X <- matrix(c(0.9999815,-2.000022,0.99973), n, 1)
 
-    erro <- 0.00005
-    n <- dim(B)[1]
-    k <- 0
-    d <- erro + 1
-    iter <- 0
+gauss_seidel <- function(A,B,xk,error){
+    xk_new <- matrix(0,3,1)
+    iter = 0;
+    n = dim(B)[1]
+    k = 0
+    d = error + 1;
+    flag = 1;  
 
-    flag <- 1  
-
+    # Test if it is possible to solve
     for (i in 1:n){
-	sum <- 0
+	sum = 0
 	for (j in 1:n){
 	    if (i != j)
-		sum <- sum + abs(A[i,j])
+		sum = sum + abs(A[i,j])
 	}
 	if (sum/abs(A[i,i]) >= 1)
-	    flag <- 0
+	    flag = 0
     }
 
-
+    # The main solution
     if (flag){
-	start_time <- Sys.time()
-	while(d > erro){
-	    k <- k + 1
-	    for (i in 1:n){
-		sum <- 0
-		for (j in 1:n){
-		    if (i != j){
-		    	if(j<i)
-		    		sum <- sum + A[i,j] * new_xk[j]
-		    	else
-		    		sum <- sum + A[i,j] * xk[j]
-		    }
-		    iter <- iter + 1
+		start_time = Sys.time()
+		while(d > error){
+		    for (i in 1:n){
+				sum = 0;
+				for (j in 1:n){
+				    if (i != j){
+				    	if(j<i)
+				    		sum = sum + A[i,j] * new_xk[j]
+				    	else
+				    		sum = sum + A[i,j] * xk[j]
+				    	}
+				    iter = iter + 1;
+					}
+				new_xk[i] = (1/A[i,i]) * (B[i]-sum);
+			    }
+
+	    max1 = max(abs(new_xk-xk));
+	    max2 = max(abs(new_xk));
+	    d = max1/max2;
+
+		xk = new_xk
 		}
-		new_xk[i] <- (1/A[i,i]) * (B[i]-sum)
-	    }
 
-	    max1 <- max(abs(new_xk-xk))
-	    max2 <- max(abs(new_xk))
-	    d <- max1/max2
-	}
+		end_time = Sys.time()
+		timer = end_time - start_time
 
-	end_time <- Sys.time()
-	timer <- end_time - start_time
-
-	result <- c(new_xk, timer, iter)
-	return (result)
+		retorno = c(new_xk, timer, iter)
+		return (retorno)
     }else 
-	return (NA)
+	cat("This cant be solved by the method");
 }
 
 A = matrix(c(10,1,2,2,5,3,1,1,10),3,3);
 B = matrix(c(7,-8,6),3,1);
 xk = matrix(c(0.7,1.4,-1.1),3, 1);
-erro = 0.00005;
+error = 0.00005;
 
-cat(gauss_seidel(A,B,xk,erro));
+cat(gauss_seidel(A,B,xk,error));
